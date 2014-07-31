2b5e0bd77fab3d86e9de03e0691252cb5e2f93e9
Peter Holme <peter@dinamo.no>
2014-07-31 13:10:05 +0200

    Added Test class

---
+++ Git Test/PHTest.h
@@ -0,0 +1,13 @@
+//
+//  PHTest.h
+//  Git Test
+//
+//  Created by Peter Holme on 31.07.14.
+//  Copyright (c) 2014 Peter Holme. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PHTest : NSObject
+
+@end
---
+++ Git Test/PHTest.m
@@ -0,0 +1,13 @@
+//
+//  PHTest.m
+//  Git Test
+//
+//  Created by Peter Holme on 31.07.14.
+//  Copyright (c) 2014 Peter Holme. All rights reserved.
+//
+
+#import "PHTest.h"
+
+@implementation PHTest
+
+@end
