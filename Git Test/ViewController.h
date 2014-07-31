d0431dfe0858404935e773ba8f3a35a457ea6b14
Peter Holme <peter@dinamo.no>
2014-07-31 12:55:57 +0200

    Initial Commit

---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Peter Holme on 31.07.14.
+//  Copyright (c) 2014 Peter Holme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Peter Holme on 31.07.14.
+//  Copyright (c) 2014 Peter Holme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
